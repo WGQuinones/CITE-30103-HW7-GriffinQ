e3f5bea0a4b225decd375c7e88fcc0a52583dfaf
WGQuinones <W.G.Quinones@tcu.edu>
2025-11-30 12:56:54 -0600

    10

---
+++ question9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[5];
+
+    printf("Enter 5 integers:\n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse in place
+    for (int i = 0; i < 5 / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[4 - i];
+        arr[4 - i] = temp;
+    }
+
+    printf("Reversed array:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
