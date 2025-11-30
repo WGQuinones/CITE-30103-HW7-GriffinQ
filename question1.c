49fae652b9311de5993fb71d932743e7ee0587bd
WGQuinones <W.G.Quinones@tcu.edu>
2025-11-30 12:37:25 -0600

    question1

---
+++ question1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+float toCelsius(float f) {
+    return (f - 32.0f) * 5.0f / 9.0f;
+}
+
+int main(void) {
+    float f, c;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &f);
+
+    c = toCelsius(f);
+    printf("Celsius: %.2f\n", c);
+
+    return 0;
+}
